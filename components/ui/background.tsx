bcdfd1a9f759a8bbd251f7c8f6d74708c2bbc085
JeffersonGudiel <jeffersongudiel@icloud.com>
2025-01-14 01:05:10 -0600

    refactor: update Background component to support theme-based styling

--- components/ui/background.tsx
+++ components/ui/background.tsx
@@ -1,20 +1,22 @@
-import React from "react";
+"use client";
 
-export function BackgroundWrapper({ children }: { children: React.ReactNode }) {
+import { useTheme } from "./ThemeProvider";
+
+export function Background() {
+  const { theme } = useTheme();
+
   return (
-    <div className="relative min-h-screen w-full bg-neutral-900">
-      <div className="absolute inset-0 bg-fuchsia-400 bg-[size:20px_20px] opacity-20 blur-[100px]"></div>
-      <div className="relative z-10">{children}</div>
+    <div className="fixed inset-0 z-[-1]">
+      {theme === "light" && (
+        <div className="relative h-full w-full bg-neutral-900">
+          <div className="absolute inset-0 bg-fuchsia-400 bg-[size:20px_20px] opacity-20 blur-[100px]"></div>
+        </div>
+      )}
+      {theme === "dark" && (
+        <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      )}
     </div>
   );
 }
 
-const background = () => {
-  return (
-    <BackgroundWrapper>
-      <div>background</div>
-    </BackgroundWrapper>
-  );
-};
-
-export default background;
+export default Background;
